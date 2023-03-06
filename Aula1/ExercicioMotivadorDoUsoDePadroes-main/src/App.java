fc756468ae5300ffee4c07fd46c3a36579b38f4a
EricoPanassol <ericopanassol@gmail.com>
2023-03-06 18:07:28 -0300

    [Feature] carro utilitário

--- Aula1/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
+++ Aula1/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
@@ -3,10 +3,14 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitário", TipoCombustivel.FLEX, 5, 70);
+
         
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
+        System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
