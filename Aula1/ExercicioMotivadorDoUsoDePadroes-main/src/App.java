11b32e44d666774c501a36ce9b454ba7677b5b5b
EricoPanassol <ericopanassol@gmail.com>
2023-03-06 18:09:16 -0300

    [Feature] fix fuel and car type fuel

--- Aula1/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
+++ Aula1/ExercicioMotivadorDoUsoDePadroes-main/src/App.java
@@ -3,7 +3,7 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        Carro utilitario = new Carro("Utilitário", TipoCombustivel.FLEX, 5, 70);
+        Carro utilitario = new Carro("Utilitário", TipoCombustivel.DIESEL, 5, 70);
 
         
 
