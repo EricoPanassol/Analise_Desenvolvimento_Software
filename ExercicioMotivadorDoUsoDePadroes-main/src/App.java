1daa682e8804000942d30374ff9c20411edeba1a
EricoPanassol <ericopanassol@gmail.com>
2023-03-13 17:20:35 -0300

    Q5

--- ExercicioMotivadorDoUsoDePadroes-main/src/App.java
+++ ExercicioMotivadorDoUsoDePadroes-main/src/App.java
@@ -1,79 +1,63 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        Carro utilitario = new Carro("Utilitário", TipoCombustivel.DIESEL, 5, 70);
-        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
+        // Criação do modelo básico com a classe Carro usando injeção de dependência
+        MotorBasico motorBasico = new MotorBasico(TipoCombustivel.GASOLINA, 10);
+        TanqueCombustivel tanqueCombustivel = new TanqueCombustivel(TipoCombustivel.GASOLINA, 55);
+        Carro basico = new Carro("Basico", motorBasico, tanqueCombustivel);
+
+        basico.abastece(TipoCombustivel.GASOLINA, 55);
+        basico.viaja(200);
+        basico.viaja(100);
+        System.out.println(basico+"\n");
+
+        // Solução da questão 1
+        MotorBasico motorEsportivo = new MotorBasico(TipoCombustivel.GASOLINA, 6);
+        TanqueCombustivel tanqueEsportivo = new TanqueCombustivel(TipoCombustivel.GASOLINA, 45);
+        Carro esportivo = new Carro("Esportivo", motorEsportivo, tanqueEsportivo);
+        esportivo.abastece(TipoCombustivel.GASOLINA, 45);
+        esportivo.viaja(200);
+        //esportivo.viaja(100);
+        System.out.println(esportivo+"\n");
+
+
+        // Solução da questão 2
+        MotorBasico motorUtilitario = new MotorBasico(TipoCombustivel.DIESEL, 5);
+        TanqueCombustivel tanqueUtilitario = new TanqueCombustivel(TipoCombustivel.DIESEL, 70);
+        Carro utilitario = new Carro("Utilitario", motorUtilitario, tanqueUtilitario);
+        utilitario.abastece(TipoCombustivel.DIESEL, 70);
+        utilitario.viaja(200);
+        utilitario.viaja(100);
+        System.out.println(utilitario+"\n");
         
-     
+        // Solução da questão 3
+        MotorBasico motorSUV = new MotorBasico(TipoCombustivel.GASOLINA, 8);
+        TanqueCombustivel tanqueSUV = new TanqueCombustivel(TipoCombustivel.FLEX, 55);
+        Carro suv = new Carro("SUV", motorSUV, tanqueSUV);
+        suv.abastece(TipoCombustivel.GASOLINA, 55);
+        suv.viaja(200);
+        suv.viaja(100);
+        System.out.println(suv+"\n");
 
-        
-        System.out.println("==========================================================================================================================================\n");
-        System.out.println("Tipos de veiculos:");
-        System.out.println("==========================================================================================================================================\n");
+        // Solução da questão 4
+        TanqueCombustivelFlex tanqueFlex = new TanqueCombustivelFlex(65);
+        MotorBasico motorSUVFlex = new MotorFlex(8,6,tanqueFlex);
+        Carro suvFlex = new Carro("SUVFlex", motorSUVFlex, tanqueFlex);
+        suvFlex.abastece(TipoCombustivel.GASOLINA, 55);
+        suvFlex.viaja(200);
+        System.out.println(suvFlex+"\n");
+        suvFlex.abastece(TipoCombustivel.GASOLINA,10);
+        suvFlex.viaja(100);
+        System.out.println(suvFlex+"\n");
 
-        System.out.println(basico);
-
-        System.out.println(basico + "\n");
-
-        basico.abastece(TipoCombustivel.GASOLINA, 999);
-        System.out.println("Abastecendo o carro " + basico.getModelo() + " com " + basico.getCombustivelDisponivel() + "L\n");
-        System.out.println("Indo para Salto do Itararé com o carro " + basico.getModelo() + "\n");
-
-        basico.viaja(15);
-        basico.viaja(32);
-
-        System.out.println(basico);
-       
-
-        System.out.println("==========================================================================================================================================\n");
-    
-        System.out.println(esportivo + "\n");
-
-        esportivo.abastece(TipoCombustivel.GASOLINA, 999);
-        System.out.println("Abastecendo o carro " + esportivo.getModelo() + " com " + esportivo.getCombustivelDisponivel() + "L\n");
-        System.out.println("Indo para a Europa com o carro " + esportivo.getModelo() + "\n");
-
-        esportivo.viaja(70);
-        esportivo.viaja(80);
-
-        System.out.println(esportivo);
-
-        System.out.println("==========================================================================================================================================\n");
-        System.out.println(utilitario + "\n");
-
-        utilitario.abastece(TipoCombustivel.DIESEL, 999);
-        System.out.println("Abastecendo o carro " + utilitario.getModelo() + " com " + utilitario.getCombustivelDisponivel() + "L");
-        System.out.println("Indo para a praia com o carro " + utilitario.getModelo());
-
-        utilitario.viaja(110);
-        utilitario.viaja(111);
-
-        System.out.println(utilitario);
-
-        System.out.println("==========================================================================================================================================\n");
-        System.out.println(suv + "\n");
-
-        suv.abastece(TipoCombustivel.FLEX, 999);
-        System.out.println("Abastecendo o carro " + suv.getModelo() + " com " + suv.getCombustivelDisponivel() + "L\n");
-        System.out.println("Indo para o Azerbaijão com o carro " + suv.getModelo() + "\n");
-
-        suv.viaja(220);
-        suv.viaja(46);
-
-        System.out.println(suv);
-
-        System.out.println("==========================================================================================================================================\n");
-        // System.out.println(suvFlex + "\n");
-
-        // suvFlex.abastece(TipoCombustivel.GASOLINA, 999);
-        // System.out.println("Abastecendo o carro " + suvFlex.getModelo() + " com " + suvFlex.getCombustivelDisponivel() + "L\n");
-        // System.out.println("Indo para o Azerbaijão com o carro " + suvFlex.getModelo() + "\n");
-
-        // suvFlex.viaja(220);
-        // suvFlex.viaja(46);
-
-        // System.out.println(suvFlex);
+        // Solução da questão 5
+        MotorBasico motorEcono = new MotorEcono();
+        TanqueCombustivel tanqueEcono = new TanqueCombustivel(TipoCombustivel.GASOLINA, 55);
+        Carro econo = new Carro("Econo", motorEcono, tanqueEcono);
+        econo.abastece(TipoCombustivel.GASOLINA, 55);
+        econo.viaja(550);
+        System.out.println(econo+"\n");
+        econo.viaja(100);
+        System.out.println(econo+"\n");
     }
 }
